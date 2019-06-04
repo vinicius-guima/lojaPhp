83a7311e8f8f634b41e0dfee08a5ca43ba84625e
vinicius-guima <vinicius_guima.social@hotmail.com>
2019-06-04 01:07:00 -0300

    fase de acabamento do projeto facul

---
+++ sobre.php
@@ -0,0 +1,79 @@
+<?php include("cabecalho.php");?>
+
+<h1 style="color: #0000FF";> Integrantes do Projeto Integrado</h1>
+
+
+<div class="bd-example" style=" max-width:500px; margin: auto;
+  border: 3px solid #FFF;" >
+  <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="3"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="4"></li>
+      <li data-target="#carouselExampleCaptions" data-slide-to="5"></li>
+    </ol>
+    <div class="carousel-inner">
+
+      <div class="carousel-item active">
+        <img src="img/vinicius.png" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Vinicius Guimarães</h5>
+          <p>Back-end com PHP e Front-end com HTML CSS BOOTSTRAP </p>
+        </div>
+      </div>
+
+      <div class="carousel-item">
+        <img src="img/henrique.png" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Henrique Silva</h5>
+          <p>Infraestrutura da virtual machine </p>
+        </div>
+      </div>
+
+      <div class="carousel-item">
+        <img src="img/jefferson.png" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Jefferson Servidoni</h5>
+          <p>Infraestrutura da virtual machine </p>
+        </div>
+      </div>
+
+      <div class="carousel-item">
+        <img src="img/cintia.png" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Cintia Rigo</h5>
+          <p>Criação e revisão do relatório</p>
+        </div>
+      </div>
+
+      <div class="carousel-item">
+        <img src="img/bruno.png" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Bruno Lazarini</h5>
+          <p>Front-end HTML CSS</p>
+        </div>
+      </div>
+
+      <div class="carousel-item">
+        <img src="img/samuel.png" class="d-block w-100" alt="...">
+        <div class="carousel-caption d-none d-md-block">
+          <h5>Samuel Rodrigues</h5>
+          <p>Back-end com PHP e MYSQL e Reclamar pra caramba</p>
+        </div>
+      </div>
+
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+</div>
+
+<?php include ("rodape.php")?>
